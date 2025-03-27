cbd03241009ffca161de299369a2dabbb1c5ce3b
Joshua Vincent Padilla <joshuapadilla706@gmail.com>
2025-03-27 23:12:49 +0800

    insights route for weekly and monthly done

--- controllers/insightsController.js
+++ controllers/insightsController.js
@@ -1,19 +1,42 @@
 import Appointment from "../models/appointmentModel.js";
 import {
   countAppointmentsByWeekday,
+  countMonthlyAppointmentsByday,
   getAppointmentServicePopularity,
-  getAppointmentsWeeklyStatus,
+  getAppointmentStatusCount,
+  newUsersThisMonth,
+  // getAppointmentStatusCount,
+  // getAppointmentsWeeklyStatus,
+  // newUsersThisMonth,
   newUsersThisWeek,
 } from "../lib/utils.js";
-import { thisWeek } from "../lib/constants.js";
+import {
+  previousMonth,
+  previousWeek,
+  thisMonth,
+  thisWeek,
+} from "../lib/constants.js";
 import User from "../models/userModel.js";
 
 export const getWeeklyAppointments = async (req, res) => {
   try {
-    const thisWeeksAppointments = await Appointment.find({
+    const { week } = req.params;
+
+    let startDate;
+    let endDate;
+
+    if (week === "currWeek") {
+      startDate = thisWeek.startDate;
+      endDate = thisWeek.endDate;
+    } else {
+      startDate = previousWeek.startDate;
+      endDate = previousWeek.endDate;
+    }
+
+    const weeklyAppointments = await Appointment.find({
       appointmentDate: {
-        $gte: thisWeek.startDate.format("YYYY-MM-DD"), // Query as string
-        $lt: thisWeek.endDate.format("YYYY-MM-DD"), // Query as string
+        $gte: startDate.format("YYYY-MM-DD"), // Query as string
+        $lt: endDate.format("YYYY-MM-DD"), // Query as string
       },
     });
 
@@ -21,32 +44,86 @@ export const getWeeklyAppointments = async (req, res) => {
 
     const totalUsers = allUsersCount.length;
 
-    const newUserCount = newUsersThisWeek(allUsersCount);
+    const newUserCount = await newUsersThisWeek(startDate, endDate);
 
-    const thisWeekServicePopularity = getAppointmentServicePopularity(
-      thisWeeksAppointments
-    );
+    const weeklyServicePopularity =
+      getAppointmentServicePopularity(weeklyAppointments);
 
-    const thisWeekStatusCount = getAppointmentsWeeklyStatus(
-      thisWeeksAppointments
-    );
+    const weeklyStatusCount = getAppointmentStatusCount(weeklyAppointments);
 
-    const thisWeeksCountsByDay = countAppointmentsByWeekday(
-      thisWeeksAppointments
-    );
+    const weeklyCountsByDay = countAppointmentsByWeekday(weeklyAppointments);
 
-    const averageAppointmentsPerWeek = thisWeeksAppointments.length / 7;
+    const averageAppointmentsPerWeek = weeklyAppointments.length / 7;
 
     res.status(200).json({
       status: "success",
       insights: {
-        numberOfWeeklyAppointment: thisWeeksAppointments.length,
-        thisWeeksCountsByDay,
-        thisWeekServicePopularity,
-        thisWeekStatusCount,
+        numberOfWeeklyAppointment: weeklyAppointments.length,
+        weeklyCountsByDay,
+        weeklyServicePopularity,
+        weeklyStatusCount,
         totalUsers,
         newUserCount,
         averageAppointmentsPerWeek,
+      },
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(400).json({
+      status: "failed",
+      message: "Server internal error",
+    });
+  }
+};
+
+export const getMonthlyAppointments = async (req, res) => {
+  try {
+    const { month } = req.params;
+
+    let startDate;
+    let endDate;
+
+    if (month === "currMonth") {
+      startDate = thisMonth.startDate;
+      endDate = thisMonth.endDate;
+    } else {
+      startDate = previousMonth.startDate;
+      endDate = previousMonth.endDate;
+    }
+
+    const monthlyAppointments = await Appointment.find({
+      appointmentDate: {
+        $gte: startDate.format("YYYY-MM-DD"), // Query as string
+        $lt: endDate.format("YYYY-MM-DD"), // Query as string
+      },
+    });
+
+    const allUsersCount = await User.find();
+
+    const totalUsers = allUsersCount.length;
+
+    const newUserCount = await newUsersThisMonth(startDate, endDate);
+
+    const monthlyServicePopularity =
+      getAppointmentServicePopularity(monthlyAppointments);
+
+    const monthlyStatusCount = getAppointmentStatusCount(monthlyAppointments);
+
+    const monthlyCountsByDay =
+      countMonthlyAppointmentsByday(monthlyAppointments);
+
+    const averageAppointmentsPerMonth = monthlyAppointments.length / 30;
+
+    res.status(200).json({
+      status: "success",
+      insights: {
+        numberOfMonthlyAppointments: monthlyAppointments.length,
+        monthlyServicePopularity,
+        monthlyCountsByDay,
+        monthlyStatusCount,
+        totalUsers,
+        newUserCount,
+        averageAppointmentsPerMonth,
       },
     });
   } catch (error) {
--- routes/insightsRoute.js
+++ routes/insightsRoute.js
@@ -1,12 +1,16 @@
 import express from "express";
 import { restrictToAdmin } from "../middleware/adminRoute.js";
-import { getWeeklyAppointments } from "../controllers/insightsController.js";
+import {
+  getMonthlyAppointments,
+  getWeeklyAppointments,
+} from "../controllers/insightsController.js";
 import { protectRoute } from "../middleware/protectRoute.js";
 
 const router = express.Router();
 
 router.use(protectRoute);
 router.use(restrictToAdmin);
-router.route("/weekly").get(getWeeklyAppointments);
+router.route("/weekly/:week").get(getWeeklyAppointments); // specify this week or previous
+router.route("/monthly/:month").get(getMonthlyAppointments);
 
 export default router;
